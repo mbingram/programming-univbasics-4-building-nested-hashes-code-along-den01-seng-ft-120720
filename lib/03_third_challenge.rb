782d97a2ae0a0cd48b65cc290257759409bcceb7
charming-input-2976 <mbingramm@gmail.com>
2020-11-25 18:46:08 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,17 +1,31 @@
 def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
+epic_tragedy = {
+  :montague => {
+    :patriarch => {
+      name: "Lord Montague",
+      age: "53"
+    },
+    :matriarch => {
+      name: "Lady Montague",
+      age: "54"
+    },
+    :hero => {
+      name: "Romeo",
+      age: "15"
+    },
+    :hero_friends => [
+      {
+        name: "Benvolio",
+        age: "17",
+        attitude: "worried"
+      },
+      {
+        name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"
+      }
+      ]
+    },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
