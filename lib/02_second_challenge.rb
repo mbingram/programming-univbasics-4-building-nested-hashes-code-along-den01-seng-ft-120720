bc9fea6b57f5ed07512ee137a08a692841d1fdb0
charming-input-2976 <mbingramm@gmail.com>
2020-11-25 18:38:16 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,8 +2,8 @@ def second_challenge
   epic_tragedy = {
   :montague => {
     :patriarch => {
-      :name => "Lord Montague",
-      :age => "53"
+      name: "Lord Montague"
+      age: "53"
   },
     :matriarch => {
       :name => "Lady Montague",
